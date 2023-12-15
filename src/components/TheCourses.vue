aa9bb0870646bd9ae5a996277052cf992562c63b
Sarvar Mannobov <keynow1290@gmail.com>
2023-12-15 16:36:29 +0500

    nothing changed 15.12.23(16:36)

--- src/components/TheCourses.vue
+++ src/components/TheCourses.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container">
-        <h1 class="text-center font-[R-500] text-6xl py-14">Популярные курсы</h1>
+        <h1 class="text-center font-[R-500] text-6xl py-14 sm:text-5xl ">Популярные курсы</h1>
 
-        <div class="image-box  flex gap-4 my-11 ">
+        <div class="image-box  flex gap-4 my-11 justify-center sm:flex flex-wrap">
             <div class="img shadow-lg shadow-slate-400/50">
                 <img src="../assets/images/pic (1).png" alt="">
                 <p class="py-2">Motion Design</p>
@@ -22,7 +22,9 @@
         </div>
 
         <div class="btns">
-            <button class="btn bg-[#10B582] py-5 px-20 justify-between text-light">Все курсы</button>
+            <button
+                class="btn flex justify-center items-center bg-[#10B582] py-5 my-8 px-20  text-light rounded-tr-3xl rounded-bl-3xl">Все
+                курсы</button>
         </div>
     </div>
 </template>
