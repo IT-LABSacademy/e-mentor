108464c0524a1b94173b519f88d4c496186228ab
Sarvar Mannobov <keynow1290@gmail.com>
2023-12-16 22:03:49 +0500

    added Learn and Mentors pages 16.12.23(22:03)

---
+++ src/components/home/TheLearn.vue
@@ -0,0 +1,42 @@
+<template>
+    <section class="TheLearn">
+        <div class="container">
+            <h1 class="text-6xl text-center py-4 font-['R-500']">На курсы вы научитесь</h1>
+            <div class="block flex justify-center items-center gap-10 py-6">
+                <div class="block1 border py-6 px-4 shadow-md">
+                    <div class="title flex items-center justify-center gap-2">
+                        <img src="/./src/assets/images/icon.svg" alt="">
+                        <h2 class="py-4 text-xl w-60">Анимировать объекты</h2>
+                    </div>
+                    <p class="text-sm">Узнаете принципы создания инфографики, персонажной 2D флэт-анимации.</p>
+                </div>
+                <div class="block2 border py-6 px-4 shadow-md">
+                    <div class="title flex items-center justify-center gap-2">
+                        <img src="/./src/assets/images/icon2.svg" alt="">
+                        <h2 class="py-4 text-xl w-60">Работать с кадром</h2>
+                    </div>
+                        <p class="text-sm">Изучите основные принципы анимации и работы с объектами, их движением в кадре.</p>
+                </div>
+                <div class="block2 border py-6 px-4 shadow-md">
+                    <div class="title flex items-center justify-center gap-2">
+                        <img src="/./src/assets/images/icon3.svg" alt="">
+                        <h2 class="py-4 text-xl w-64">Работать в After Effects</h2>
+                    </div>
+                    <p class="text-sm">Научитесь реализовывать ваши идеи с помощью популярной программы для анимации</p>
+                </div>
+                <div class="block3 border py-6 px-4 shadow-md">
+                    <div class="title flex items-center justify-center gap-2">
+                        <img src="/./src/assets/images/icon4.svg" alt="">
+                        <h2 class="py-4 text-xl w-60">Работать со звуком</h2>
+                    </div>
+                    <p class="text-sm">Освоите инструменты работы, сможете редактировать музыку
+                        в своем проекте.</p>
+                </div>
+            </div>
+            <div class="btns">
+                <button class="btn flex justify-center items-center bg-[#10B582] py-5 my-8 px-20  text-light rounded-tr-3xl rounded-bl-3xl">Программа курса
+                </button>
+            </div>
+        </div>
+    </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,23 +2,24 @@
   <section class="main bg-green">
    <HomeNavbar />
   </section>
-  <!-- <TheNavbar /> -->
   <HomeNumber />
   <TheCourses />
   <HomeNumber />
   <TrainingFormat />
+  <TheMentorsVue />
+  <TheLearn />
 </template>
 
 <script>
-// import AboutNumber from '@/components/AboutNumber.vue';
 import HomeNumber from '../components/home/HomeNumber.vue';
 import HomeNavbar from '@/components/home/homeNavbar.vue';
 import TheCourses from '../components/TheCourses.vue';
 import TrainingFormat from '../components/home/TrainingFormat.vue'
-
+import TheMentorsVue from '../components/TheMentors.vue'
+import TheLearn from '@/components/home/TheLearn.vue';
 
 export default {
-  components:{HomeNumber, HomeNavbar, TheCourses, TrainingFormat}
+  components:{HomeNumber, HomeNavbar, TheCourses, TrainingFormat, TheMentorsVue, TheLearn}
 }
 
 
